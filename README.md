77992cf8b0ca9ca8889ea8514b42e89f63d0ccc4
ShinchanThakur <rohanthakurarmy@gmail.com>
2022-01-18 19:42:44 +0530

    Created Basic Table

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # Learning React Tables
 > I have made this project to learn about react tables
 
+## Techs To Learn
+> I will be learning these techs in this project
+<img alt="Techs To Learn" src="images/techsToLearn.png">
+
 ## Mock Data
 > A mock data is taken for our use case from mockaroo.com with following settings
 <img width="555" alt="Mockaroo" src="images/mockaroo.png">
+
+## Basic Table
+> Follow the following steps to create a table in react.
+<img width="555" alt="Steps To Make Table" src="images/stepsToMakeTable.png">
---
+++ src/components/BasicTable.js
@@ -0,0 +1,52 @@
+import React, {useMemo} from 'react'
+import {useTable} from 'react-table'
+import MOCK_DATA from './MOCK_DATA.json'
+import {COLUMNS} from './columns'
+import './table.css'
+
+export const BasicTable = () => {
+
+    const columns = useMemo(() => COLUMNS, [])
+    const data = useMemo(() => MOCK_DATA, [])   //UseMemo hook prevents recreation of data after every render
+                                                //If we wont use this, then react table will think that new data is received and on every render, it will do a lot of calculations
+
+    const tableInstance = useTable({  //This is a hook, It is basically a function
+        columns,    //This is columns:columns
+                    //ES6 shorthand syntax allows us to write only columns instead of this
+        data
+    }) //It recommends memoising data before feeding it to the hook
+    
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow
+    } = tableInstance
+
+    return (
+        <table {...getTableProps()}>
+            <thead>
+                {headerGroups.map((headerGroup) => (
+                    <tr {...headerGroup.getHeaderGroupProps()}>
+                        {headerGroup.headers.map((column) => (
+                            <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+                        ))}
+                    </tr>
+                ))}
+            </thead>
+            <tbody {...getTableBodyProps()}>
+                {rows.map((row) => {
+                    prepareRow(row)
+                    return (
+                        <tr {...row.getRowProps()}>
+                            {row.cells.map((cell) => {
+                                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                            })}
+                        </tr>
+                    )
+                })}
+            </tbody>
+        </table>
+    )
+}
